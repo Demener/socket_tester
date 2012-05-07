f700595ce395a1b8202ad5cfc019839d3150094f
Demener <dnowak@DNowak.local>
2012-05-07 14:16:29 -0400

    Changed Socket Host to use TCPServer.

--- socket_host.rb
+++ socket_host.rb
@@ -1,20 +1,30 @@
-require 'eventmachine'
+require 'socket'
 require 'yaml'
 
-module EchoServer
-  def post_init
-    puts "-- someone connected to the echo server!"
+class SocketTesterHost
+  attr_accessor :sock
+  def launch_server (port=2000)
+    puts "Launching Server on Port " + port.to_s + "."
+    @sock= TCPServer.open(port)
+    loop{
+      client =  @sock.accept
+      puts "Client Connected."
+      data =  client.read
+      puts data
+      write_to_file(data)
+      client.close
+      puts "Closing Connection."
+    }
   end
-
-  def receive_data data
-    # send_data ">>>you sent: #{data}"
-    puts data
-    write_to_file(data)
-    close_connection if data =~ /quit/i
+  
+  def write_port s_send
+    puts "Sending: " + s_send
+    @sock.write(s_send)
   end
-
-  def unbind
-    puts "-- someone disconnected from the echo server!"
+  
+  def close_port
+    @sock.close
+    puts "Connection Closed"
   end
   
   def write_to_file s_data
@@ -32,26 +42,28 @@ module EchoServer
     end
     File.open(filename,"a") {|f| f.write(data.to_yaml) }
   end
-end
-
-if (ARGV.length > 0)
-  # Load the IP address and Port from the YAML file.
-  config_name = ARGV[0]
-  config = YAML::load (File.open(config_name))
-  ip_addr = config.to_a[0][1] 
-  port = config.to_a[1][1]
-else
-  # No Config File loaded, run with defaults and place in config file.
-  puts "No Arguments passed in. ARGV[0] = Config File. ARGV[1] = Log File."
-  puts "Using defaults: Config file: Config.yml, Log File: Incoming.yml."
-  ip_addr = "127.0.0.1"
-  port = 2000
-  config_name = "Config.yml"
-  config_data = {"IP" => ip_addr, "Port" => port}
   
-  File.open(config_name,"w") {|f| f.write(config_data.to_yaml) }
+  def load_config arg
+    if (arg.length > 0)
+      # Load the IP address and Port from the YAML file.
+      config_name = arg[0]
+      config = YAML::load (File.open(config_name))
+      ip_addr = config.to_a[0][1] 
+      port = config.to_a[1][1]
+    else
+      # No Config File loaded, run with defaults and place in config file.
+      puts "No Arguments passed in. ARGV[0] = Config File. ARGV[1] = Log File."
+      puts "Using defaults: Config file: Config.yml, Log File: Incoming.yml."
+      ip_addr = "127.0.0.1"
+      port = 2000
+      config_name = "Config.yml"
+      config_data = {"IP" => ip_addr, "Port" => port}
+
+      File.open(config_name,"w") {|f| f.write(config_data.to_yaml) }
+    end
+    launch_server(port)
+  end
 end
 
-EventMachine::run {
-  EventMachine::start_server ip_addr, port, EchoServer
-}
+mytester = SocketTesterHost.new
+mytester.load_config(ARGV)
